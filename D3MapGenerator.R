eababc7c095dc9f49cf2636a7d98db1d03fa5a6e
Andy <yc230@duke.edu>
2020-12-17 16:56:09 -0800

    improvements to the D3 map

--- D3MapGenerator.R
+++ D3MapGenerator.R
@@ -8,7 +8,7 @@ D3Map <- data.frame(state = avgCases$Group.1,
                     seven_day_average_Tests = round(avgTests$x), 
                     seven_day_average_death_toll = round(avgDeaths$x))
 
-D3Map$seven_day_positivity_rate <- D3Map$seven_day_average_Positive / D3Map$seven_day_average_Tests
+D3Map$seven_day_positivity_rate <- round(D3Map$seven_day_average_Positive / D3Map$seven_day_average_Tests * 100, 2)
 D3Map <- D3Map[!D3Map$state %in% c('AS','GU','MP','PR','VI'),]
 D3Map$state_name <- abbr2state(D3Map$state)
 write.csv(D3Map, 'D3Map.csv', row.names = FALSE)
